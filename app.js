0c3eb41808bec1c2527a5dc006bb11edb75f06ca
Caleb Nkunze <nkunzecaleb@gmail.com>
2020-01-22 10:02:54 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,5 @@
-const http = require('http'); const hostname = '127.0.0.1';
-const port = 3000; const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World\n');
-}); server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
+const server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+const server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+server.listen(server_port, server_ip_address, () => {
+    console.log("Hello, your Listening on " + server_ip_address + ", Port " + server_port);
 });
